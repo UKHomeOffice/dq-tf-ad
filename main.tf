3924ce88b36fc8ce45c8aa1b7f627234272f4b00
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-06 13:30:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -170,8 +170,8 @@ EOF
 
 resource "aws_vpc_dhcp_options" "dns_resolver" {
   domain_name_servers = [
-    "${aws_directory_service_directory.ad.dns_ip_addresses}",
-    "10.1.0.2",
+    "AmazonProvidedDNS",
+    "${aws_directory_service_directory.ad.dns_ip_addresses}"
   ]
 }
 
